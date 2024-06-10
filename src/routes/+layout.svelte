1d3d63508f06dafaa7848285531865c26a4f1020
PD <89239979+PDgaming@users.noreply.github.com>
2024-06-10 14:08:40 +0530

    Added basic information to get the website started

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,7 +6,24 @@
 	<main>
 		<slot />
 	</main>
+
+	<footer>
+		<p>*It's Still in Development</p>
+	</footer>
 </div>
 
 <style>
+	footer {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		padding: 12px;
+	}
+
+	@media (min-width: 480px) {
+		footer {
+			padding: 12px 0;
+		}
+	}
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,5 +5,11 @@
 	<title>Home</title>
 </svelte:head>
 
+<center>
+	<h1>Official Cracked Eggs Website</h1>
+	<h2>Join our Minecraft Server at</h2>
+	<h2>Cracked_eggs.aternos.me</h2>
+</center>
+
 <style>
 </style>
