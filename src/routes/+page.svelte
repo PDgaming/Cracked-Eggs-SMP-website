a6166f56b4f2203f87c32d8bcff9ba51d74c151a
PD <89239979+PDgaming@users.noreply.github.com>
2024-06-10 14:10:59 +0530

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,7 +8,7 @@
 <center>
 	<h1>Official Cracked Eggs Website</h1>
 	<h2>Join our Minecraft Server at</h2>
-	<h2>Cracked_eggs.aternos.me</h2>
+	<h2><u>Cracked_eggs.aternos.me </u></h2>
 </center>
 
 <style>
