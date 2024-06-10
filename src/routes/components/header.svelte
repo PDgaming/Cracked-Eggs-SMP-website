e5e3936f7528b5953fb7e6e7accf2976149797aa
PD <89239979+PDgaming@users.noreply.github.com>
2024-06-10 15:17:07 +0530

    Create header.svelte

---
+++ src/routes/components/header.svelte
@@ -0,0 +1,13 @@
+<script>
+    import { goto } from "$app/navigation";
+
+    function home() {
+        goto("/");
+    }
+    function play() {
+        goto("/play");
+    }
+</script>
+
+<button on:click={home}>Home</button>
+<button on:click={play}>Play Now</button>
